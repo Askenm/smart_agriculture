c9ea0fa92abaf8df140aac4a80b2c13a98c1fa63
askm <askm@itu.dk>
2023-12-01 09:00:19 +0100

    rarranging structure for studpid reason

--- fctryengine-test_w_outputs/date_parser.py
+++
@@ -1,6 +0,0 @@
-def to_date(number):
-    x = number/60%24
-    decimal = int((x-int(str(x).split('.')[0]))*60)
-    if len(str(decimal))==1:
-        decimal = f"0{decimal}"
-    return f"{int(x)}:{decimal}"
